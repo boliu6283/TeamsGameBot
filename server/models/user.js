9f1e7649e030f7df4d4b0cedac48a37b814397cc
boliu6283 <liu.6283@osu.edu>
2020-06-20 15:34:09 -0700

    sturcture refactor

--- server/models/user.js
+++ server/models/user.js
@@ -5,9 +5,11 @@ const userSchema = new Schema({
   aad: { type: String, required: true },
   email: { type: String },
   name: { type: String, required: true },
+  role: { type: String, default: 'User' },
   status: { type: String, default: 'offline' },
   score: { type: Number, default: 10 },
   description: { type: String, default: '' },
+  subscription: { type: Boolean, default: true },
   gamePurchase: { type: Schema.Types.ObjectId, ref: 'gamePurchase' },
   createdAt: { type: Date },
   updatedAt: { type: Date }
