64240b5e3fdf06d74472fe20f9e322f62b4082df
boliu6283 <liu.6283@osu.edu>
2020-06-28 16:00:31 -0700

    session model update

--- models/gameSession.js
+++ models/gameSession.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 const gameSessionSchema = new Schema({
   code: { type: String, required: true },
   game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
+  status: { type: String, default: 'await', enum(await, start, complete) },
+  url: { type: String },
   host: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   players: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   audiences: [{ type: Schema.Types.ObjectId, ref: 'User' }],
