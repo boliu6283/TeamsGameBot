8beba32dd19b79789ff8545d3d1f440a4e2dadd6
Zitong Wei <weizitongbupt@gmail.com>
2020-06-28 16:00:40 -0700

    Creating session

--- dialogs/createSession.js
+++ dialogs/createSession.js
@@ -10,7 +10,6 @@ class CreateSessionDialog extends ComponentDialog {
 
     this._luisRecognizer = luisRecognizer;
 
-    this.addDialog(new ChoicePrompt(constants.CREATE_SESSION_CARD_PROMPT));
     this.addDialog(new WaterfallDialog(constants.CREATE_SESSION_WATERFALL_DIALOG, [
         this.createSessionCardStep.bind(this)
     ]));
@@ -19,16 +18,38 @@ class CreateSessionDialog extends ComponentDialog {
   }
 
   async createSessionCardStep(stepContext) {
-    // Clean up CreateSessionCard
-    // CreateSessionCard.body = [];
+    // Find the chosen game.
+    //
+    const chosenGame = stepContext.options.gameChoice;
+    const gameInfo = await Resolvers.game.getGameByName({ gameName: chosenGame });
 
+    // Get host info.
+    //
+    const userInfo = stepContext.options.user;
+
+    // Generate code
+    //
+    const roomCode = generateCode();
+
+    // Render session creation card. 
+    // TODO: add title for room code.
     let createSessionCard = CardFactory.adaptiveCard(CreateSessionCard);
+    createSessionCard.content.body[0].items[0].columns[0].items[0].text = gameInfo.name;
+    createSessionCard.content.body[0].items[0].columns[0].items[1].url = gameInfo.profile;
+    createSessionCard.content.body[0].items[0].columns[0].items[2].text = roomCode.toString();
 
-    return await stepContext.prompt(constants.CREATE_SESSION_CARD_PROMPT, {
-      prompt: MessageFactory.attachment(createSessionCard),
-      choices: ['Create Room']
-    });
+    await stepContext.context.sendActivity({ attachments: [createSessionCard] });
+    await stepContext.context.sendActivity('Creating session...');
+    
+    Resolvers.gameSession.createSession()
   }
+}
+
+function generateCode() {
+  return 'xxxx'.replace(/[x]/g, function(c) {
+     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+     return v.toString(16);
+  });
 }
 
 module.exports = {
