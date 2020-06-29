50b4091c8809ec6cf9e3013c3506e3e0cb5130fc
Hanzhang Zeng <hazeng@microsoft.com>
2020-06-28 18:44:17 -0700

    Implemented Create Room Session

--- resolvers/gameSession.js
+++ resolvers/gameSession.js
@@ -1,10 +1,21 @@
 const GameSession = require('../models/gameSession');
 
 const getSession = async (args) => {
-  return await GameSession.findOne({ 
-    code: args.code 
+  return await GameSession.findOne({
+    code: args.code
   }).populate({
-    path: 'game host'
+    path: 'game host players'
+  });
+}
+
+const addPlayerToSession = async (args) => {
+  const { code, userId } = args;
+  await GameSession.findOneAndUpdate({
+    code
+  }, {
+    $addToSet: {
+      players: userId
+    }
   });
 }
 
@@ -29,6 +40,7 @@ const deleteSession = async (args) => {
 
 module.exports = {
   getSession,
+  addPlayerToSession,
   createSession,
   deleteSession
 };
--- resolvers/proactiveMessage.js
+++ resolvers/proactiveMessage.js
@@ -1,24 +1,41 @@
 const { TurnContext } = require('botbuilder-core');
 const BotAdapter = require('./botAdapter');
+const GameSession = require('./gameSession');
 
-const conversationReferences = {};
+let _conversationReferences = {};
 
-async function notifyAll(message) {
+const notifyIndividual = async (userAad, message) => {
   const adapter = BotAdapter.getInstance();
-  for (const conversationReference of Object.values(conversationReferences)) {
-    await adapter.continueConversation(conversationReference, async turnContext => {
+  if (_conversationReferences[userAad]) {
+    const cref = _conversationReferences[userAad];
+    await adapter.continueConversation(cref, async turnContext => {
       await turnContext.sendActivity(message);
     });
   }
 }
 
-function addConversationReference(context) {
-  const conversationReference = TurnContext.getConversationReference(context.activity);
-  this.conversationReferences[conversationReference.user.aadObjectId] = conversationReference;
+// sessionCode = RoomNumber, RoomCode, SessionCode
+const notifySession = async (sessionCode, message) => {
+  const session = await GameSession.getSession({ code: sessionCode });
+  session.players.forEach(async (p) => {
+    await notifyIndividual(p.aad, message);
+  });
+}
+
+const notifyAll = async (message) => {
+  Object.keys(_conversationReferences).forEach(async (k) => {
+    await notifyIndividual(k, message);
+  });
+}
+
+const addConversationReference = (context) => {
+  const cref = TurnContext.getConversationReference(context.activity);
+  _conversationReferences[cref.user.aadObjectId] = cref;
 }
 
 module.exports = {
-  conversationReferences,
   addConversationReference,
-  notifyAll
+  notifyAll,
+  notifySession,
+  notifyIndividual
 }
