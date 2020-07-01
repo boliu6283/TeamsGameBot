97e2f1674f359a068f8d839feabfde54f85cd1cc
boliu6283 <liu.6283@osu.edu>
2020-07-01 16:02:24 -0700

    path fix

--- dialogs/games/spyfall/spyfallDialog.js
+++ dialogs/games/spyfall/spyfallDialog.js
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ComponentDialog, WaterfallDialog, ChoicePrompt } = require("botbuilder-dialogs");
-const { CardFactory, MessageFactory } = require("botbuilder-core");
-const constants = require("../config/constants");
-const SpyfallRoles = require("../static/spyfall.json");
-const SpyfallCard = require("../static/spyfallCard.json");
-const Resolvers = require("../resolvers");
+const { ComponentDialog, WaterfallDialog, ChoicePrompt } = require('botbuilder-dialogs');
+const { CardFactory, MessageFactory } = require('botbuilder-core');
+const constants = require('../../../config/constants');
+const SpyfallRoles = require('../../../static/spyfall.json');
+const SpyfallCard = require('../../../static/spyfallCard.json');
+const Resolvers = require('../../../resolvers');
 
 class SpyfallDialog extends ComponentDialog {
   constructor(luisRecognizer) {
@@ -24,8 +24,8 @@ class SpyfallDialog extends ComponentDialog {
 
     for (let i = 0; i < SpyfallRoles.locations.length; i++) {
       const locationItem = {
-        type: "TextBlock",
-        text: SpyfallRoles.locations[i],
+        type: 'TextBlock',
+        text: SpyfallRoles.locations[i]
       };
       if (i % 2 == 0) {
         SpyfallCard.body[1].columns[0].items.push(locationItem);
@@ -95,15 +95,15 @@ class SpyfallDialog extends ComponentDialog {
     session.players.forEach(async (player, index) => {
       const playerCard = CardFactory.adaptiveCard(SpyfallCard);
       if (index == spyIndex) {
-        playerCard.content.body[3].text = "Role: Spy";
-        playerCard.content.body[2].text = "Location: (empty, try guess)";
+        playerCard.content.body[3].text = 'Role: Spy';
+        playerCard.content.body[2].text = 'Location: (empty, try guess)';
       } else {
         playerCard.content.body[3].text =
-          "Role: " +
+          'Role: ' +
           SpyfallRoles[
             `location.${location}.role${((index + spyIndex) % 7) + 1}`
           ];
-        playerCard.content.body[2].text = "Location: " + location;
+        playerCard.content.body[2].text = 'Location: ' + location;
       }
       await Resolvers.proactiveMessage.notifyIndividualCard(
         player.aad,
@@ -113,31 +113,31 @@ class SpyfallDialog extends ComponentDialog {
 
     let hostSpyfallCard = CardFactory.adaptiveCard(SpyfallCard);
     if (spyIndex == session.players.length) {
-      hostSpyfallCard.content.body[3].text = "Role: Spy";
-      hostSpyfallCard.content.body[2].text = "Location: (empty, try guess)";
+      hostSpyfallCard.content.body[3].text = 'Role: Spy';
+      hostSpyfallCard.content.body[2].text = 'Location: (empty, try guess)';
     } else {
       hostSpyfallCard.content.body[3].text =
-        "Role: " +
+        'Role: ' +
         SpyfallRoles[`location.${location}.role${(spyIndex % 7) + 1}`];
-      hostSpyfallCard.content.body[2].text = "Location: " + location;
+      hostSpyfallCard.content.body[2].text = 'Location: ' + location;
     }
 
     return await stepContext.prompt(constants.SPYFALL_PROMPT, {
       prompt: MessageFactory.attachment(hostSpyfallCard),
-      choices: ["🕹️StartAnotherMatch", "📖EndGame"],
+      choices: ['🕹️StartAnotherMatch', '📖EndGame']
     });
   }
 
   async restartStep(stepContext) {
     switch (stepContext.result.value) {
-      case "🕹️StartAnotherMatch": {
+      case '🕹️StartAnotherMatch': {
         return await stepContext.replaceDialog(
           constants.SPYFALL_DIALOG,
           stepContext.options
         );
       }
 
-      case "📖EndGame": {
+      case '📖EndGame': {
         return await stepContext.endDialog();
       }
     }
