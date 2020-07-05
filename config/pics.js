31da73466e82f8ea3aa046efd10df3cad1272463
Yuki <35462917+tsiyuki@users.noreply.github.com>
2020-07-04 18:02:32 -0700

    add main menu picture (#4)
    Co-authored-by: Chenrui Shu <chens@microsoft.com>

--- config/pics.js
+++ config/pics.js
@@ -1,6 +1,5 @@
 const menuPics = [
-  'https://gamebot.blob.core.windows.net/menu-pic/cat_1.jpg',
-  'https://gamebot.blob.core.windows.net/menu-pic/cat_2.jpg'
+  'https://gamebot.blob.core.windows.net/menu-pic/TeamsGameBot.jpg'
 ];
 
 const rankPics = [
