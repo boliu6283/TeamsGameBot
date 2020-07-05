50bf2d80ecabd1d635d072229b89972d2f76df2c
boliu6283 <liu.6283@osu.edu>
2020-07-05 01:03:32 -0700

    guess dialog up

---
+++ dialogs/games/spyfall/spyfallGuessDialog.js
@@ -0,0 +1,33 @@
+const { Dialog } = require('botbuilder-dialogs');
+const Resolvers = require('../../../resolvers');
+const constants = require('../../../config/constants');
+
+class SpyfallGuessDialog extends Dialog {
+  constructor(luisRecognizer) {
+    super(constants.SPYFALL_GUESS_DIALOG);
+
+    this._luisRecognizer = luisRecognizer;
+  }
+
+  async beginDialog(dc, options) {
+    const { spyGuess, location, sessionCode } = dc.context.activity.value;
+    await Resolvers.countdown.kill(sessionCode);
+    if (spyGuess.toLowerCase() === location) {
+      await Resolvers.proactiveMessage.notifySession(
+        sessionCode,
+        `Spyfall ${sessionCode} is now finished, spy win`
+      );
+    } else {
+      await Resolvers.proactiveMessage.notifySession(
+        sessionCode,
+        `Spyfall ${sessionCode} is now finished, players win`
+      );
+    }
+
+    return await dc.endDialog();
+  }
+}
+
+module.exports = {
+  SpyfallGuessDialog
+};
