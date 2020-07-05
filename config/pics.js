57b982e14a248ac46d0152d34c8e379c535f8387
Zitong Wei <weizitongbupt@gmail.com>
2020-07-04 23:57:06 -0700

    correct pic

--- config/pics.js
+++ config/pics.js
@@ -1,5 +1,5 @@
 const menuPics = [
-  'https://gamebot.blob.core.windows.net/menu-pic/TeamsGameBot.jpg'
+  'https://gamebot.blob.core.windows.net/menu-pic/TeamsGameBot.png'
 ];
 
 const rankPics = [
