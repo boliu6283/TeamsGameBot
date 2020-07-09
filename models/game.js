bb1678708608e52aeda93658cc71b4efc6fc71ea
Jiaxun Song <jiaxsong@microsoft.com>
2020-07-09 10:29:39 -0700

    move spyfall static json to MongoDB

--- models/game.js
+++ models/game.js
@@ -4,12 +4,14 @@ const { Schema } = mongoose;
 const gameSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, default: '' },
+  rules: { type: String, default: '' },
   capacity: [{ type: Number }],
   price: { type: Number, default: 0 },
   profile: { type: String },
   pictures: [{ type: String }],
   createdAt: { type: Date },
-  updatedAt: { type: Date }
+  updatedAt: { type: Date },
+  metadata: { type: Object }
 }, {
   timestamps: true
 });
